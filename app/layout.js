043608fc31310f83523ce3d24f1f871819a2be60
Sveta-ls <slana6570@gmail.com>
2025-12-17 13:16:46 +0300

    to see if it works

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./my.css";
+import "./fonts.css";
+import Header from "./components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,9 +21,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <div>{children}</div>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,66 +1,27 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Header from "./components/Header.js";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div>
+      <Header />
+      <body className="body">
+        <section className="main_sec">
+          <span className="h_text_home">Привет, друг!</span>
+          <p className="p_text_home">
+            На связи команда дизайна СС ИИТММ, год почти закончился, а это
+            значит время подвести итоги, что-то ещё там ляляля пупуупукпккпв.
           </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+          <Link href="/about">
+            <button className="button_main">посмотреть статистику!</button>
+          </Link>
+        </section>
+      </body>
+      <footer className="gifts">
+        <img src="/pink_gift.svg"></img>
+        <img src="/red_gift.svg"></img>
+        <img src="/yellow_gift.svg"></img>
+      </footer>
     </div>
   );
 }
