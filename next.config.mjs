f27f80bff3f20156191804102a6948e1adcbf6ee
Sveta-ls <slana6570@gmail.com>
2025-12-17 13:42:04 +0300

    to see if it works5

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true, // Для GitHub Pages
   },
-  basePath: process.env.NODE_ENV === "production" ? "/wow" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/wow/" : "",
+  basePath: "/wow", // Для GitHub Pages
+  assetPrefix: "/wow",
+  trailingSlash: true, // Добавьте это!
 };
 
 export default nextConfig;
