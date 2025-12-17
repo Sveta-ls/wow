8f21989289c23dfc029560298892a7ca26f7d5d0
Sveta-ls <slana6570@gmail.com>
2025-12-17 13:30:35 +0300

    to see if it works2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export", // Важно: экспортировать статический сайт
+  images: {
+    unoptimized: true, // Для GitHub Pages
+  },
+  basePath: process.env.NODE_ENV === "production" ? "/wow" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/wow/" : "",
 };
 
 export default nextConfig;
